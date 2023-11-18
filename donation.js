824f9e5d3d8d4c28d50598faf2265d7958d7fc96
LIONBABYCRYPTO <lionbabycrypto@gmail.com>
2023-11-18 16:29:31 +0800

    v1.3

--- donation.js
+++ donation.js
@@ -39,3 +39,54 @@ function signTx() {
     .then((r) => document.getElementById("result").innerText=JSON.stringify(r,null,4))
     .catch((e) => console.log("error:" + e.message));
 }
+
+var addressLabel = document.getElementById("loggedIn");
+var address = "";
+
+var itemDiv = document.getElementById("item");
+
+function login() {
+  vendor
+    .sign("cert", {
+      purpose: "identification",
+      payload: {
+        type: "text",
+        content: "Please sign the certificate to continue purchase"
+      }
+    })
+    // .accepted(() => alert("accepted"))
+    .request()
+    .then((r) => {
+      address = r.annex.signer;
+      addressLabel.innerText = "Logged In: " + address;
+      itemDiv.style.display = "inline-block";
+    })
+    .catch((e) => console.log("error:" + e.message));
+}
+
+function logout() {
+  addressLabel.innerText = "Please login";
+  itemDiv.style.display = "none";
+  address = "";
+}
+
+function buy() {
+  var txIdLabel = document.getElementById("txidLabel");
+  vendor
+    .sign("tx", [
+      {
+        to: "0x70aE85A2fF6030366F512DbcD60Be3828139b498",
+        value: 1 * 1e18, //unit in wei
+        data: "0x"
+      }
+    ])
+  //enforce signer
+  .signer(address)
+    //a link that can redirect user to visit
+    .link("https://explore-testnet.vechain.org/transactions/{txid}")
+    .comment("buy skill stats: connex skill +2")
+    // .accepted(() => alert("accepted"))
+    .request()
+    .then((r) => (txIdLabel.innerText = "TxId: " + r.txid))
+    .catch((e) => console.log("error:" + e.message));
+}
